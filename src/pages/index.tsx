d6a062eabd75881e013889584ae66023d0ba6386
Onur Ozcan <onur@9fin.com>
2023-03-25 00:02:03 +0000

    Add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,14 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
 
-  console.log(user);
+  const { data } = api.posts.getAll.useQuery(undefined, {
+    retry: 0,
+    refetchOnWindowFocus: false,
+  });
 
+  console.log(data);
   return (
     <>
       <Head>
@@ -19,6 +22,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{user.isSignedIn ? <SignOutButton /> : <SignInButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
